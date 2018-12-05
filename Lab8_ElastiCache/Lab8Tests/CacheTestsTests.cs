c2f75ff5f1e54fbd8f403c8df67c10d8c9a32bf8
Ethan Chen <ethan96.chen@gmail.com>
2018-12-05 22:13:20 +0800

    AWS labs
    AWS labs

---
+++ Lab8_ElastiCache/Lab8Tests/CacheTestsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8.Tests
+{
+    [TestClass()]
+    public class CacheTestsTests
+    {
+        [TestMethod()]
+        public void TestElastiCacheMethodTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
